67c11f1ea1dda39fafae43fca263e7a274028253
MRadim0 <a.szkutnik19@gmail.com>
2025-02-04 19:23:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function handleNoClick() {
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
-    }
+}
+
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
